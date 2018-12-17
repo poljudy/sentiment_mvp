2f060dc22d4338c74957b53fe2b20895f99bd1d5
poljudy <franklinjin229@gmail.com>
2018-12-17 22:44:25 +0100

    admin classes for current models

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Publisher, Article
 
-# Register your models here.
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ["name", "domain"]
+    search_fields = ["name", "domain"]
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ["title", "publisher", "status"]
+    list_select_related = ["publisher"]
+    list_filter = ["status"]
+    date_hierarchy = "published_ts"
+    search_fields = ["title", "uri"]
--- feeds/admin.py
+++ feeds/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Feed, FeedItem
 
-# Register your models here.
+
+@admin.register(Feed)
+class FeedAdmin(admin.ModelAdmin):
+    list_display = ["title", "publisher", "last_fetch_ts"]
+    list_select_related = ["publisher"]
+
+
+@admin.register(FeedItem)
+class FeedItem(admin.ModelAdmin):
+    list_display = ["title", "feed", "fetched_ts", "published_ts"]
+    list_select_related = ["feed"]
+    search_fields = ["title", "uri"]
