7c18b3ef41d22b2d4fb1fdad4c42092eed42ccdb
poljudy <franklinjin229@gmail.com>
2018-12-18 02:21:36 +0100

    parsing all the feeds

--- feeds/logic.py
+++ feeds/logic.py
@@ -11,35 +11,41 @@ from .models import Feed, FeedItem
 logger = logging.getLogger(__name__)
 
 
-class ParseFeed:
-    """Parses feed's rss page and saves new entries to the db.
+class ParseFeeds:
+    """Parses feeds rss pages and saves new entries to the db.
     """
 
-    def __init__(self, feed: Feed):
+    def __init__(self):
         """Set feed instance as attribute.
 
         Args:
             feed: Feed object from the db
         """
-        self.feed = feed
-        self.fetch_feed_items()
+        self.parse_feeds()
 
-    def fetch_feed_items(self):
+    def parse_feeds(self):
+        """Go through all the feeds in the system and fetch new articles.
+        """
+        for feed in Feed.objects.all():
+            self.fetch_feed_items(feed)
+
+    def fetch_feed_items(self, feed):
         """Go through each item in feed and save it if new.
         """
-        live_feed = feedparser.parse(self.feed.uri)
+        live_feed = feedparser.parse(feed.uri)
         for item in live_feed.entries:
-            self.save_single_item(item)
+            self.save_single_item(feed, item)
 
-        self.feed.last_fetch_ts = pendulum.now("UTC")
-        self.feed.save()
+        feed.last_fetch_ts = pendulum.now("UTC")
+        feed.save()
 
-    def save_single_item(self, item: feedparser.FeedParserDict) -> FeedItem:
+    def save_single_item(self, feed: Feed, item: feedparser.FeedParserDict) -> FeedItem:
         """Save single item from rss feed into db as FeedItem instance.
 
         If item is newly created also created accompanying article.
 
         Args:
+            feed: Feed object from the db
             item: parsed feedparser single item
 
         Returns:
@@ -50,7 +56,7 @@ class ParseFeed:
         feed_item, feed_item_created = FeedItem.objects.get_or_create(
             uri=item.link,
             defaults={
-                "feed": self.feed,
+                "feed": feed,
                 "title": item.title,
                 "published_ts": item_published,
             },
