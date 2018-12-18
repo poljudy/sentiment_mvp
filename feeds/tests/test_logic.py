280f9cc5fbdc8bbf54df37008098f914014cff3d
poljudy <franklinjin229@gmail.com>
2018-12-18 03:19:44 +0100

    additional tests for parsing feeds

--- feeds/tests/test_logic.py
+++ feeds/tests/test_logic.py
@@ -3,11 +3,71 @@ from django.test import TestCase
 from content.models import Article
 from content.tests.factories import ArticleFactory
 from feeds.logic import ParseFeeds
+from feeds.models import FeedItem
 from feeds.tests.factories import FeedFactory, FeedItemFactory
 from feedparser import FeedParserDict
 
 
 class ParseFeedsTestCase(TestCase):
+    def test_save_single_item__is_created_with_article(self):
+        feed = FeedFactory()
+
+        item = FeedParserDict()
+        item["published"] = "Mon, 17 Dec 2018 16:19:00 -0500"
+        item["title"] = "Dummy title"
+        item["link"] = "https://example.com/article.html"
+
+        parser = ParseFeeds()
+
+        RESULT = parser.save_single_item(feed, item)
+
+        assert isinstance(RESULT, FeedItem)
+        assert RESULT.uri == "https://example.com/article.html"
+        assert RESULT.title == "Dummy title"
+        assert RESULT.feed == feed
+
+        assert Article.objects.get(uri="https://example.com/article.html")
+
+    def test_save_single_item__is_created_existing_article(self):
+        ArticleFactory(uri="https://example.com/article.html")
+
+        feed = FeedFactory()
+
+        item = FeedParserDict()
+        item["published"] = "Mon, 17 Dec 2018 16:19:00 -0500"
+        item["title"] = "Dummy title"
+        item["link"] = "https://example.com/article.html"
+
+        parser = ParseFeeds()
+
+        RESULT = parser.save_single_item(feed, item)
+
+        assert isinstance(RESULT, FeedItem)
+        assert RESULT.uri == "https://example.com/article.html"
+        assert RESULT.title == "Dummy title"
+        assert RESULT.feed == feed
+
+    def test_save_single_item__is_existing(self):
+        feed = FeedFactory()
+        feed_item = FeedItemFactory(feed=feed)
+
+        item = FeedParserDict()
+
+        item["published"] = pendulum.instance(
+            feed_item.published_ts
+        ).to_rfc1123_string()
+        item["title"] = feed_item.title
+        item["link"] = feed_item.uri
+
+        parser = ParseFeeds()
+
+        RESULT = parser.save_single_item(feed, item)
+
+        assert isinstance(RESULT, FeedItem)
+        assert RESULT.uri == feed_item.uri
+        assert RESULT.title == feed_item.title
+        assert RESULT.feed == feed_item.feed
+
     def test_get_single_item_published_time__with_timezone(self):
         EXPECTED_RESULT = pendulum.datetime(
             2018, 12, 17, 16, 19, 00, tz="America/New_York"
