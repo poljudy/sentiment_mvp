e5c4f6a52baf2ae7cedffc66b7fc3ca1d7e30eba
poljudy <franklinjin229@gmail.com>
2018-12-18 19:03:11 +0100

    set sentiment score as part of content parsing pipeline

--- content/logic.py
+++ content/logic.py
@@ -18,87 +18,6 @@ from .models import Publisher, Article
 logger = logging.getLogger(__name__)
 
 
-class ParseContent:
-    """Parses saved article content.
-
-    Use .parse_publishers() method to start parsing all publishers articles.
-    """
-
-    def parse_publishers(self):
-        """Go through all the publishers in the system and parse new articles.
-        """
-        for publisher in Publisher.objects.all():
-            self.parse_newly_created_articles(publisher)
-
-    def parse_newly_created_articles(self, publisher: Publisher):
-        """Go through all articles and fetch their content.
-
-        Args:
-            publisher: Publisher db instance
-        """
-        for article in publisher.articles.filter(status=Article.STATUS_CREATED):
-            self.parse_new_article(article)
-
-    def parse_new_article(self, article: Article):
-        """Fetch content for a single article.
-
-        Args:
-            article: Article db instance with STATUS_CREATED
-        """
-        response = self.get_article_raw_response(article.uri)
-
-        if response:
-            doc = Document(response.text)
-
-            article.content_source = response.text
-            article.content_clean = self.clean_content(doc.summary())
-
-            article.save()
-
-            article.set_status(Article.STATUS_CONTENT_FETCHED)
-        else:
-            article.set_status(Article.STATUS_RESPONSE_ERROR)
-
-    def get_article_raw_response(self, uri: str) -> Optional[requests.Response]:
-        """Get response from the publisher for given uri.
-
-        Args:
-            uri: full uri to the article
-
-        Returns:
-            Response object or None
-        """
-        try:
-            response = requests.get(uri)
-
-            if response.status_code == 200:
-                return response
-            else:
-                logger.exception(
-                    f"Status code {response.status_code} returned for article {uri}"
-                )
-                return None
-        except ConnectionError:
-            logger.exception(f"Failed to make a connection to host when fetching {uri}")
-            return None
-
-    def clean_content(self, content: str) -> str:
-        """Clean content from html tags.
-
-        Args:
-            content: dirty dirty html content
-
-        Returns:
-            content cleaned from html tags
-        """
-        # clean from tags
-        clean = strip_tags(content)
-        # clean from all the extra whitespace
-        clean = " ".join(clean.split())
-
-        return clean
-
-
 class SentimentCalculate:
     """Calculates sentiment score for given article.
 
@@ -152,3 +71,94 @@ class SentimentCalculate:
             self.article.set_status(Article.STATUS_SENTIMENT_SET)
         else:
             self.article.set_status(Article.STATUS_SENTIMENT_ERROR)
+
+
+class ParseContent:
+    """Parses saved article content.
+
+    Use .parse_publishers() method to start parsing all publishers articles.
+    """
+
+    def parse_publishers(self):
+        """Go through all the publishers in the system and parse new articles.
+        """
+        for publisher in Publisher.objects.all():
+            self.parse_newly_created_articles(publisher)
+
+    def parse_newly_created_articles(self, publisher: Publisher):
+        """Go through all articles and fetch their content.
+
+        Args:
+            publisher: Publisher db instance
+        """
+        for article in publisher.articles.filter(status=Article.STATUS_CREATED):
+            self.parse_new_article(article)
+
+    def parse_new_article(self, article: Article):
+        """Fetch content for a single article.
+
+        Args:
+            article: Article db instance with STATUS_CREATED
+        """
+        response = self.get_article_raw_response(article.uri)
+
+        if response:
+            doc = Document(response.text)
+
+            article.content_source = response.text
+            article.content_clean = self.clean_content(doc.summary())
+
+            article.save()
+
+            article.set_status(Article.STATUS_CONTENT_FETCHED)
+
+            self.get_article_sentiment_score(article)
+        else:
+            article.set_status(Article.STATUS_RESPONSE_ERROR)
+
+    def get_article_sentiment_score(self, article: Article):
+        """Calculate sentiment score for article.
+
+        Args:
+            article: Article instance with clean content set
+        """
+        SentimentCalculate(article).get_and_set_sentiment()
+
+    def get_article_raw_response(self, uri: str) -> Optional[requests.Response]:
+        """Get response from the publisher for given uri.
+
+        Args:
+            uri: full uri to the article
+
+        Returns:
+            Response object or None
+        """
+        try:
+            response = requests.get(uri)
+
+            if response.status_code == 200:
+                return response
+            else:
+                logger.exception(
+                    f"Status code {response.status_code} returned for article {uri}"
+                )
+                return None
+        except ConnectionError:
+            logger.exception(f"Failed to make a connection to host when fetching {uri}")
+            return None
+
+    def clean_content(self, content: str) -> str:
+        """Clean content from html tags.
+
+        Args:
+            content: dirty dirty html content
+
+        Returns:
+            content cleaned from html tags
+        """
+        # clean from tags
+        clean = strip_tags(content)
+        # clean from all the extra whitespace
+        clean = " ".join(clean.split())
+
+        return clean
